02f57e52d73c3f3d665da0101f6784f045eccef7
classifiedfear <classifiedfear@gmail.com>
2024-11-26 21:48:58 +0100

    small fix

--- src/tg_bot_float_common_dtos/update_db_scheduler_dtos/source_data_tree_dto.py
+++ src/tg_bot_float_common_dtos/update_db_scheduler_dtos/source_data_tree_dto.py
@@ -6,7 +6,6 @@ from tg_bot_float_common_dtos.schema_dtos.agent_dto import AgentDTO
 from tg_bot_float_common_dtos.schema_dtos.glove_dto import GloveDTO
 from tg_bot_float_common_dtos.schema_dtos.quality_dto import QualityDTO
 from tg_bot_float_common_dtos.update_db_scheduler_dtos.agent_relation_dto import AgentRelationDTO
-from tg_bot_float_common_dtos.update_db_scheduler_dtos.agent_relations_dto import AgentRelationsDTO
 from tg_bot_float_common_dtos.update_db_scheduler_dtos.glove_relation_dto import GloveRelationDTO
 from tg_bot_float_common_dtos.update_db_scheduler_dtos.relation_dto import RelationDTO
 from tg_bot_float_common_dtos.schema_dtos.skin_dto import SkinDTO
@@ -20,5 +19,5 @@ class SourceDataTreeDTO(BaseModel):
     gloves: List[GloveDTO]
     agents: List[AgentDTO]
     relations: List[RelationDTO]
-    glove_relations: List[GloveRelationsDTO]
-    agent_relations: List[AgentRelationsDTO]
+    glove_relations: List[GloveRelationDTO]
+    agent_relations: List[AgentRelationDTO]
